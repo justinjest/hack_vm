136dc5dc80d233fd934cfcb2bf0ff5d4cfac2762
Jessica Cayaban <jest.cayaban@gmail.com>
2025-08-04 20:30:33 +0900

    Working on parsing more

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,156 @@
+use std::collections::HashMap;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum CommandType {
+    Arithmetic,
+    Push,
+    Pop,
+}
+
+
+#[derive(Debug, PartialEq)]
+struct LineParsing {
+    ctype: CommandType,
+    arg1: String, // This is either the arithmetic type, or the location
+    arg2: Option<i32>, // This will be the target of offsets or the num
+}
+
+impl LineParsing {
+    fn new(line: Vec<&str>) -> Self {
+        let mapping = HashMap::from([
+            ("push", CommandType::Push),
+            ("pop", CommandType::Pop),
+            ("add", CommandType::Arithmetic),
+            ("sub", CommandType::Arithmetic),
+        ]);
+
+        let location = HashMap::from([
+            ("local", "lcl"),
+            ("argument", "arg"),
+            ("this", "THIS"),
+            ("that", "THAT"),
+            ("static", "STATIC"),
+            ("R13", "R13"),
+            ("R14", "R14"),
+            ("R15", "R15"),
+            ("constant", "CONSTANT"),
+            ("temp", "TEMP"),
+        ]);
+
+        let ctype = mapping[line[0]];
+        let mut arg1 = line[0].to_string();
+        let mut arg2: Option<i32> = None;
+        if line.len() >= 3 {
+            print!("Original: {}\n", line[1]);
+            println!("New: {}\n", location[line[1]]);
+            arg1 = location[line[1]].to_string();
+            arg2 = Some(line[2].parse()
+                        .expect("Unable to parse num {line[2]}\n"));
+        }
+        LineParsing{ ctype: ctype, arg1: arg1, arg2: arg2 }
+    }
+
+    fn push_constant(&self) -> String {
+        println!("{:?}", self);
+       format!("@{:?}
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1", self.arg2.unwrap()).to_string()
+    }
+
+    fn push(&self) -> String {
+        println!("{:?}", self);
+        let mut res = "".to_string();
+
+        match &self.arg1[..] {
+            "STATIC" => res = self.push_constant(),
+            _ => return "".to_string(),
+        }
+
+        res
+    }
+
+    fn pop(&self) -> String {
+        return "".to_string()
+    }
+
+    fn add(&self) -> String {
+        return "".to_string()
+    }
+
+    fn sub(&self) -> String {
+        return "".to_string()
+    }
+
+}
+
+fn split_line(line: &str) -> Vec<&str> {
+    line.split(" ")
+        .collect()
+}
+
+
 fn main() {
     println!("Hello, world!");
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn split_line_push() {
+        let c = split_line("push local 1");
+        let e = ["push", "local", "1"];
+        assert_eq!(c, e);
+    }
+
+    #[test]
+    fn split_line_arithmetic() {
+        let c = split_line("add");
+        let e = ["add"];
+        assert_eq!(c, e);
+    }
+
+    #[test]
+    fn line_parsing_test0() {
+        let tmp = Vec::from(["pop", "local", "1"]);
+        let c = LineParsing::new(tmp);
+        let e = LineParsing { ctype: CommandType::Pop,
+        arg1: "lcl".to_string(),
+        arg2: Some(1)};
+        assert_eq!(c, e);
+    }
+
+    #[test]
+    fn line_parsing_test1() {
+        let tmp = Vec::from(["add"]);
+        let c = LineParsing::new(tmp);
+        let e = LineParsing {
+            ctype: CommandType::Arithmetic,
+            arg1: "add".to_string(),
+            arg2: None,
+        };
+        assert_eq!(c, e);
+    }
+
+    #[test]
+    fn line_test0() {
+        let tmp = Vec::from(["push", "static", "1"]);
+        let c = LineParsing::new(tmp).push();
+        let e = "@1
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1".to_string();
+
+        assert_eq!(c, e);
+    }
+
 }
